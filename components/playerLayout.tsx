118261b407f5146d0587bdd7f371b01bf172e32b
Jake O'Toole <jake.otoole@nytimes.com>
2021-12-13 11:18:29 -0600

    Added 3 components for our basic layout.

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,9 +1,22 @@
 import { Box } from "@chakra-ui/layout";
 
 const PlayerLayout = ({ children }) => (
-  <Box>
-    Layout
-    {children}
+  <Box width="100vw" height="100vh">
+    <Box
+      position="absolute"
+      width="250px"
+      top="0"
+      left="0"
+      border="1px solid black"
+    >
+      Sidebar
+    </Box>
+    <Box marginLeft="250px" marginBottom="100px">
+      {children}
+    </Box>
+    <Box postion="absolute" bottom="0" left="0" border="1px solid black">
+      Player
+    </Box>
   </Box>
 );
 export default PlayerLayout;
